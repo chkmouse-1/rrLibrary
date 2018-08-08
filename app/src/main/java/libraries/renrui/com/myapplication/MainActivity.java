c63028259d3b85aac9ffac12db909758e7a38e68
DESKTOP-HSPCSUU\xuelei <1291522088@qq.com>
2018-08-08 19:10:31 +0800

    code format

--- app/src/main/java/libraries/renrui/com/myapplication/MainActivity.java
+++ app/src/main/java/libraries/renrui/com/myapplication/MainActivity.java
@@ -13,25 +13,10 @@ import com.renrui.libraries.util.UtilitySecurity;
 
 public class MainActivity extends Activity {
 
-    TextView tvTest;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        tvTest = findViewById(R.id.tvTest);
-
-        String text = "asdfasdfasdfsajdfl啊啊啊啊啊askdfjl88";
-        String hotword = "啊啊啊啊啊";
-        UtilityControl.setHotWordsText(tvTest, text, hotword, R.color.red_4d52, new ITextviewClickable() {
-            @Override
-            public void onSpanClick() {
-                int sd = 90;
-                sd++;
-            }
-        });
     }
 }
