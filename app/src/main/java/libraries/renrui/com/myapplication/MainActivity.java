950eec497badb961c2adb6692db3b862321bd262
xuelei <iloveunix@qq.com>
2019-01-21 15:44:32 +0800

    test

--- app/src/main/java/libraries/renrui/com/myapplication/MainActivity.java
+++ app/src/main/java/libraries/renrui/com/myapplication/MainActivity.java
@@ -2,17 +2,12 @@ package libraries.renrui.com.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
-import com.renrui.libraries.interfaces.IHttpDownloadInterFace;
-import com.renrui.libraries.util.CustomToast;
-import com.renrui.libraries.util.LibUtility;
-import com.renrui.libraries.util.UtilitySecurity;
-import com.renrui.libraries.util.mHttpClient;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -20,32 +15,30 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
+    private TextView tvTest1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, TestActivity.class);
-
-        float f = 1.2f;
-        UtilitySecurity.putExtras(intent, "float", f);
-
-        double d = 2.2d;
-        UtilitySecurity.putExtras(intent, "double", d);
-
-        int in = 1;
-        UtilitySecurity.putExtras(intent, "int", in);
-
-        String s = "string .";
-        UtilitySecurity.putExtras(intent, "string", s);
-
-        ArrayList<String> alString = new ArrayList<>();
-        alString.add("aaa");
-
-        ArrayList<Integer> alInteger = new ArrayList<>();
-        alInteger.add(22);
-
-        int sd = 90;
-        sd++;
+//        tvTest1 = findViewById(R.id.tvTest1);
+//
+//        String text = "asdfas123456dfasdfas4567dfasdf";
+//        String[] arrHotWord = new String[]{"12345", "4567"};
+//        UtilityControl.setHotWordsText(tvTest1, text, arrHotWord, R.color.red_3f42, new ITextviewClickable() {
+//            @Override
+//            public void onSpanClick(int position) {
+//                int sd = 90;
+//                sd++;
+//
+//                if (position == 0)
+//                    CustomToast.makeTextWarn("aaaa");
+//                else
+//                    CustomToast.makeTextWarn("bbbb");
+//
+////                LibUtility.getAppOnlySign(MainActivity.this);
+//            }
+//        });
     }
 }
