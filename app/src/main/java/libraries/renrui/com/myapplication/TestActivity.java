8b6a593774e49cd1959fccfa5847b05520dde2b9
xuelei <iloveunix@qq.com>
2019-01-21 15:55:08 +0800

    尝试注释 toast

--- app/src/main/java/libraries/renrui/com/myapplication/TestActivity.java
+++ app/src/main/java/libraries/renrui/com/myapplication/TestActivity.java
@@ -12,18 +12,18 @@ public class TestActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomToast.makeTextError("TestActivity ... start");
-//        ToastCompat.makeText(this, "hello world!", Toast.LENGTH_SHORT).show();
-
-        try {
-
-            Thread.sleep(5000);
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-
-        CustomToast.makeTextSucess("TestActivity... end");
+//        CustomToast.makeTextError("TestActivity ... start");
+////        ToastCompat.makeText(this, "hello world!", Toast.LENGTH_SHORT).show();
+//
+//        try {
+//
+//            Thread.sleep(5000);
+//        }
+//        catch (Exception ex)
+//        {
+//            ex.printStackTrace();
+//        }
+//
+//        CustomToast.makeTextSucess("TestActivity... end");
     }
 }
