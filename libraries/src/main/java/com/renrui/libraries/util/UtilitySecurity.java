28924a7ec6ae8d9ffaeabd06a87cce9d704b2fdb
xiaoniudadi <niupuyue@aliyun.com>
2019-06-10 17:06:18 +0800

    添加contains安全方法，其中set和hashmap只能是String类型的

--- libraries/src/main/java/com/renrui/libraries/util/UtilitySecurity.java
+++ libraries/src/main/java/com/renrui/libraries/util/UtilitySecurity.java
@@ -74,6 +74,36 @@ public class UtilitySecurity {
         return TextUtils.equals(a, b);
     }
 
+    public static boolean contains(String sourc, String tag) {
+        if (isEmpty(sourc) || isEmpty(tag)) return false;
+        return sourc.contains(tag);
+    }
+
+    public static boolean contains(Set<String> set, String tag) {
+        if (isEmpty(set) || isEmpty(tag)) return false;
+        try {
+            for (String obg : set) {
+                if (equals(obg, tag)) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean contains(HashMap<String, ?> maps, String tag) {
+        if (isEmpty(maps) || isEmpty(tag)) return false;
+        Set<String> keys = null;
+        try {
+            keys = maps.keySet();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return contains(keys, tag);
+    }
+
     public static boolean equalsIgnoreCase(String a, String b) {
         if (a == null && b == null)
             return true;
