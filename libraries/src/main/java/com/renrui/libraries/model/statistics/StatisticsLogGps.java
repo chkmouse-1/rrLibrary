2826acc78ac30fa53c684e98cba281128e8d30ec
xuelei <iloveunix@qq.com>
2019-06-20 16:35:29 +0800

    code format

--- libraries/src/main/java/com/renrui/libraries/model/statistics/StatisticsLogGps.java
+++ libraries/src/main/java/com/renrui/libraries/model/statistics/StatisticsLogGps.java
@@ -4,9 +4,9 @@ package com.renrui.libraries.model.statistics;
  * GPS定位 实时日志
  */
 public class StatisticsLogGps extends StatisticsLogBaseContent {
-    public String city = "";//:"北京",       //城市
+    public String city = "";//城市
     public String address = "";//:"中关村文焕创意产业园", // 详细地址
-    public long latitude; //纬度
-    public long longitude;//经度
-    public String altitude = "";//海拔
+    public double latitude; //纬度
+    public double longitude;//经度
+    public double altitude;//海拔
 }
